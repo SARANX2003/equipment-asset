7f2ab007219ae60f763a0981dbd30e4b0ae79596
SARANX2003 <6504305001323@student.sru.ac.th>
2026-02-18 11:15:37 +0700

    fix mongodb prerender crash

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -2,10 +2,6 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI");
-}
-
 let cached = global.mongoose;
 
 if (!cached) {
@@ -13,6 +9,10 @@ if (!cached) {
 }
 
 export default async function dbConnect() {
+  if (!MONGODB_URI) {
+    throw new Error("Please define MONGODB_URI");
+  }
+
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
